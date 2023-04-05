56cf174954f3793e7b700df7485c49a2da46878d
moonrockk <119200481+moonrockk@users.noreply.github.com>
2023-04-05 22:17:27 +0300

    Update solenoid.sh

--- solenoid.sh
+++ solenoid.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 snap install docker
-docker pull moonrockkk/safari:16.0
-docker pull selenoid/video-recorder
+screen -d -m docker pull moonrockkk/safari:16.0
+screen -d -m docker pull selenoid/video-recorder
 wget https://github.com/aerokube/selenoid/releases/download/1.10.10/selenoid_linux_amd64 -O /home/cm
 chmod +x ./home/cm
 mkdir /home/config
@@ -10,4 +10,3 @@ wget https://raw.githubusercontent.com/moonrockk/WebKit_Pro/main/browsers.json -
 wget https://raw.githubusercontent.com/moonrockk/WebKit_Pro/main/solenoid.service -O /lib/systemd/system/selenoid.service
 systemctl daemon-reload
 systemctl enable selenoid.service
-reboot
